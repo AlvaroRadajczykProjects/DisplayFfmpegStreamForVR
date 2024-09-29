38ec60dac744132bb5276b3dc5d39ce6818c155a
Alvaroradajczyk <alvaradasan@gmail.com>
2024-09-29 21:06:32 +0200

    Draw one view next to other

--- app/src/main/java/com/example/displayffmpegstreamforvr/MainActivity.kt
+++ app/src/main/java/com/example/displayffmpegstreamforvr/MainActivity.kt
@@ -1,41 +1,70 @@
 package com.example.displayffmpegstreamforvr
 
 import android.os.Bundle
-import android.widget.FrameLayout
+import android.view.TextureView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.viewinterop.AndroidView
+import com.example.displayffmpegstreamforvr.renderer.ReplicatingRendererFactory
 import com.example.displayffmpegstreamforvr.util.WifiUtil
 import com.google.android.exoplayer2.C
 import com.google.android.exoplayer2.DefaultLivePlaybackSpeedControl
 import com.google.android.exoplayer2.DefaultLoadControl
+import com.google.android.exoplayer2.DefaultRenderersFactory
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.LoadControl
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.PlaybackParameters
+import com.google.android.exoplayer2.RenderersFactory
 import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
-import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DataSource
 import com.google.android.exoplayer2.upstream.DefaultAllocator
 import com.google.android.exoplayer2.upstream.DefaultLoadErrorHandlingPolicy
 import com.google.android.exoplayer2.upstream.LoadErrorHandlingPolicy
 import com.google.android.exoplayer2.upstream.UdpDataSource
+import kotlin.math.ceil
 
 class MainActivity : ComponentActivity() {
     private lateinit var exoPlayer: ExoPlayer
 
+    private lateinit var drawView: TextureView
+    private lateinit var toCopyViews: List<TextureView>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        drawView = TextureView(this)
+        toCopyViews = listOf(TextureView(this))
+
+        //Create two texture views renderer
+        val rendererFactory: RenderersFactory = ReplicatingRendererFactory(
+            this,
+            drawView,
+            toCopyViews
+        )
+
         // Initialize the ExoPlayer
         exoPlayer = ExoPlayer.Builder(this).setLoadControl(createLowBufferLoadControl())
-            .setLivePlaybackSpeedControl(createLivePlaybackSpeedControl()).setTrackSelector(createTrackSelector()).build()
+            .setRenderersFactory(rendererFactory)
+            .setLivePlaybackSpeedControl(createLivePlaybackSpeedControl())
+            .setTrackSelector(createTrackSelector())
+            .build()
 
         // Set faster playback speed to reduce buffering as much as possible
         exoPlayer.playbackParameters = PlaybackParameters(1.02f)
@@ -44,16 +73,25 @@ class MainActivity : ComponentActivity() {
             // Load the UDP Stream and prepare the player
             val streamUrl = "udp://$ipAddress:1234"
             val mediaSource = buildUdpMediaSource(streamUrl)
-            Toast.makeText(this, "Ip address: $streamUrl", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Ip address: $ipAddress", Toast.LENGTH_LONG).show()
             // Start the ExoPlayer with UDP MediaSource
             exoPlayer.setMediaSource(mediaSource)
+            exoPlayer.setVideoTextureView(drawView)
             exoPlayer.prepare()
             exoPlayer.playWhenReady = true
         }
 
         // Set up the content with Jetpack Compose
         setContent {
-            UDPStreamPlayer(exoPlayer)
+            UDPStreamPlayerView(
+                drawView,
+                toCopyViews,
+                //Change this to your video stream dimensions
+                //Both views must fit in screen. Otherwise, only one will be displayed
+                1020,
+                574,
+                12
+            )
         }
     }
 
@@ -124,17 +162,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun UDPStreamPlayer(exoPlayer: ExoPlayer) {
-    // Composable to display ExoPlayer's PlayerView
-    AndroidView(modifier = Modifier.fillMaxSize(), factory = { context ->
-        // Create a PlayerView and attach the ExoPlayer to it
-        PlayerView(context).apply {
-            player = exoPlayer
-            layoutParams = FrameLayout.LayoutParams(
-                FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT
+fun UDPStreamPlayerView(
+    drawnView: TextureView,
+    toCopyViews: List<TextureView>,
+    width: Int,
+    height: Int,
+    distance: Int
+) {
+    val sep = ceil(((distance * 1f) / 2f).toDouble()).toFloat()
+    val density = LocalDensity.current
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        AndroidView(
+            modifier = Modifier
+                .width(with(density) { width.toDp() })
+                .height(with(density) { height.toDp() })
+                .padding(end = with(density) { sep.toDp() })
+                .background(Color.Black),
+            factory = { _ -> drawnView },
+            update = {}
+        )
+        toCopyViews.forEach { view ->
+            AndroidView(
+                modifier = Modifier
+                    .width(with(density) { width.toDp() })
+                    .height(with(density) { height.toDp() })
+                    .padding(start = with(density) { sep.toDp() })
+                    .background(Color.Black),
+                factory = { _ -> view },
+                update = {}
             )
         }
-    }, update = {
-        it.player = exoPlayer // Update the player if needed
-    })
+    }
 }
